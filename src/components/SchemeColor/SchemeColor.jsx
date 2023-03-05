9196ea8993024341b47b24e79a3605c8b0e1d30f
Michaela Křenková <misakren@gmail.com>
2023-03-05 23:16:05 +0100

    Hotový úkol

---
+++ src/components/SchemeColor/SchemeColor.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import './style.css';
+
+export const SchemeColor = ({color}) =>
+    <div className="scheme-color"  style={{backgroundColor: color}}>
+        {color}
+    </div>
